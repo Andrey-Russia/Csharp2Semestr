4becb8bfd091d77899a8fb3f5bc63a3da4eba7da
Andrew <126329757+Andrey-Russia@users.noreply.github.com>
2026-01-15 08:35:09 +0300

    п

---
+++ Планировщик бюджета/Планировщик бюджета/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Напишите ваш доход: ");
+        double income = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Расходы на аренду: ");
+        double rent = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Расходы на еду: ");
+        double food = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Расходы на транспорт: ");
+        double transport = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("На сколько недель необходимо рассчитать бюджет? ");
+        int weeks = Convert.ToInt32(Console.ReadLine());
+
+        double totalTrats = rent + food + transport;
+
+        double remainingMoney = income - totalTrats;
+
+        double weeklyBudget = remainingMoney / weeks;
+
+        if (weeklyBudget >= 100)
+            Console.WriteLine($"Бюджет комфортный. Остаток на неделю: {weeklyBudget:F2}");
+        else if (weeklyBudget >= 50 && weeklyBudget <= 99.99)
+            Console.WriteLine($"Бюджет средний, нужно экономить. Остаток на неделю: {weeklyBudget:F2}");
+        else Console.WriteLine($"Бюджет очень жесткий, придется сильно экономить. Остаток на неделю: {weeklyBudget:F2}");
+    }
+}
