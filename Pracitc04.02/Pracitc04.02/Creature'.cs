19ad3866c7d361a467f4b295a51274b8cf70be21
Andrew <126329757+Andrey-Russia@users.noreply.github.com>
2026-02-06 18:36:46 +0300

    1

---
+++ Pracitc04.02/Pracitc04.02/Creature'.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+internal abstract class Creature
+{
+    protected int Damage { get; private set; }
+
+    public virtual int TakeDamage()
+    {
+        return Damage;
+    }
+}
+
+internal class Goblin : Creature
+{
+    protected int PoisonDamage { get; private set; }
+
+    public override int TakeDamage()
+    {
+        return base.TakeDamage() + PoisonDamage;
+    }
+}
+internal class Ork : Creature
+{
+    protected int CritRate { get; set; }
+
+    public override int TakeDamage()
+    {
+        return base.TakeDamage() * CritRate;
+    }
+}
+
+internal class Dragon : Creature
+{
+    protected int DamageArea { get; set; }
+    protected int FireDamage { get; set; }
+
+    public override int TakeDamage()
+    {
+        return base.TakeDamage() + DamageArea + FireDamage;
+    }
+}
---
+++ Pracitc04.02/Pracitc04.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Goblin goblin = new Goblin { Damage = 10, PoisonDamage = 5 };
+        Ork ork = new Ork { Damage = 20, CritRate = 2 };
+        Dragon dragon = new Dragon { Damage = 100, DamageArea = 20, FireDamage = 10 };
+
+
+        int goblinDamage = goblin.TakeDamage();
+        int orfDamage = ork.TakeDamage();
+        int dragonDamage = dragon.TakeDamage();
+
+        Console.WriteLine($"{goblinDamage}");
+        Console.WriteLine($"{orfDamage}");
+        Console.WriteLine($"{dragonDamage}");
+    }
+}
